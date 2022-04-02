76187a50c2a03c57d79de37d812e0adb591cdd26
Dan Farrow <dan@zellement.com>
2022-04-02 15:16:09 +0100

    tweaks

--- src/components/_Layout/Header/index.js
+++ src/components/_Layout/Header/index.js
@@ -1,32 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import { motion } from "framer-motion"
+import { RiMenu3Line } from "react-icons/ri"
+import { IoMdClose } from "react-icons/io"
 import Navigation from "../Navigation"
 
 export default function Header(props) {
   const { siteTitle } = props || {}
 
+  const [mobileNavOpen, setMobileNavOpen] = useState(false)
+
   return (
     <header className="py-8 md:py-16">
       <div className="container">
-        <div className="flex flex-wrap items-center mb-4">
-          <motion.button
-            className="block w-5 h-5 mr-3 bg-black rounded"
-            animate={{ rotate: 180 }}
-            transition={{
-              loop: Infinity,
-              duration: 2,
-              ease: "anticipate",
-            }}
-          />
-
+        <div className="flex items-center justify-between w-full mb-4">
           <Link className="block text-lg font-bold md:text-xl" to="/">
             {siteTitle}
           </Link>
-        </div>
 
-        <Navigation />
+          <Navigation
+            mobileNavOpen={mobileNavOpen}
+            setMobileNavOpen={setMobileNavOpen}
+          />
+        </div>
       </div>
+
+      <button
+        aria-label="Navigation menu button"
+        tabIndex={0}
+        role="button"
+        className={`bg-black text-white absolute top-0 right-0 z-50 p-2 m-4 lg:hidden`}
+      >
+        {mobileNavOpen ? (
+          <IoMdClose
+            className="text-2xl"
+            onClick={() => {
+              setMobileNavOpen(!mobileNavOpen)
+            }}
+          />
+        ) : (
+          <RiMenu3Line
+            className="text-2xl"
+            onClick={() => {
+              setMobileNavOpen(!mobileNavOpen)
+            }}
+          />
+        )}
+      </button>
     </header>
   )
 }
--- src/components/_Layout/Navigation/index.js
+++ src/components/_Layout/Navigation/index.js
@@ -1,43 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
-import { RiMenu3Line } from "react-icons/ri"
-import { IoMdClose } from "react-icons/io"
 
-export default function Navigation() {
+export default function Navigation(props) {
+  const { mobileNavOpen } = props || {}
+
   return (
-    <nav className="lg:flex lg:content-end lg:justify-end lg:flex-col">
-      <ul
-        className={
-          "absolute inset-0 z-50 w-full h-full bg-white md:block md:relative md:inset-auto md:h-auto"
-        }
-      >
+    <nav
+      className={`absolute p-8 inset-0 z-40 w-full transform h-full bg-white lg:translate-x-0 lg:w-auto lg:relative lg:h-auto lg:flex lg:content-end lg:justify-end lg:flex-col ${
+        mobileNavOpen ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
+      <ul className={"flex flex-col lg:flex-row gap-4"}>
         <li>
           <Link className="" activeClassName="is-active" to="/">
             Home
           </Link>
+        </li>
+        <li>
           <Link className="" activeClassName="is-active" to="/about/">
             About
           </Link>
         </li>
       </ul>
-
-      <div
-        aria-label="Navigation menu button"
-        tabIndex={0}
-        role="button"
-        className={`bg-black text-white absolute top-0 right-0 z-40 p-2 md:hidden`}
-      >
-        <RiMenu3Line className="text-2xl" />
-      </div>
-
-      <div
-        aria-label="Navigation close menu"
-        tabIndex={0}
-        role="button"
-        className={`bg-black text-white absolute top-0 right-0 z-50 p-2 md:hidden`}
-      >
-        <IoMdClose className="text-2xl" />
-      </div>
     </nav>
   )
 }
