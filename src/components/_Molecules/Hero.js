df15d10aa268680c78c74ed468edc812720a2f1c
Dan Farrow <dan@zellement.com>
2022-12-23 14:01:13 +0000

    go back btn

--- src/components/_Molecules/Hero.js
+++ src/components/_Molecules/Hero.js
@@ -1,5 +1,6 @@
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import React from "react"
+import {BsFillArrowLeftCircleFill} from "react-icons/bs"
 
 export default function Hero({ title, subtitle, category, goBack, isHome }) {
   return (
@@ -10,6 +11,12 @@ export default function Hero({ title, subtitle, category, goBack, isHome }) {
           {subtitle ? <h2 className="m-0 ">{subtitle}</h2> : null}
         </div>
         {isHome ? null : (
+          <>
+          <div className="col-span-12">
+          <button onClick={() => navigate(-1)} className="flex flex-row items-center w-auto gap-2 p-1 pl-1 pr-4 mt-4 transition-all duration-300 border border-navy-500 text-navy-500 rounded-3xl hover:bg-butterscotch-500">
+            <BsFillArrowLeftCircleFill className="text-lg" /> <span className="font-mono text-2xs whitespace-nowrap">Go Back</span>
+          </button>
+          </div>
           <nav className="col-span-12 mt-4">
             <ul className="flex flex-row gap-2 text-2xs">
               <li className="flex flex-row items-center gap-2">
@@ -39,6 +46,7 @@ export default function Hero({ title, subtitle, category, goBack, isHome }) {
               )}
             </ul>
           </nav>
+          </>
         )}
       </div>
     </section>
