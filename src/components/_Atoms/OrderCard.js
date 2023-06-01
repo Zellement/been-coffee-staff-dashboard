b92740ce99275ec8b21dec79551c0c31292add63
Dan Farrow <dan@zellement.com>
2023-06-01 18:58:22 +0100

    ui changes

--- src/components/_Atoms/OrderCard.js
+++ src/components/_Atoms/OrderCard.js
@@ -7,8 +7,24 @@ import { BiHash } from "react-icons/bi"
 
 export default function OrderCard({ item }) {
   return (
-    <div className="flex flex-col flex-shrink-0 w-3/4 p-8 bg-white shadow-lg rounded-xl lg:w-1/4">
-      <div className="relative flex flex-row items-center justify-between w-full h-40 p-2 mb-8 border border-gray-100">
+    <div className="flex flex-col flex-shrink-0 w-3/4 p-4 bg-white shadow-lg rounded-xl lg:w-auto lg:flex-1 lg:p-8">
+      <div className="relative flex flex-row items-center justify-between w-full h-32 p-2 mb-8 border border-gray-100">
+        <div className="text-3xs">
+          <div className="flex flex-row items-center gap-2">
+            <h4 className="text-tuscany"><RiTruckFill /></h4>
+            <span>{item.node.expectedDeliveryDate}</span>
+          </div>
+          <div className="flex flex-row items-center gap-2 opacity-50">
+            <h4 className="text-tuscany"><RiMoneyPoundBoxFill /></h4>
+            <span>{item.node.orderDate}</span>
+          </div>
+          {item.node.orderId ?
+            <div className="flex flex-row items-center gap-2">
+              <h4 className="text-tuscany"><BiHash /></h4>
+              <span className="">{item.node.orderId}</span>
+            </div>
+            : null}
+        </div>
         {item.node.supplier.logo ?
           <GatsbyImage
             className="object-contain w-full h-full"
@@ -17,26 +33,10 @@ export default function OrderCard({ item }) {
           /> :
           <span>{item.node.supplier.name}</span>
         }
-        <div className="text-3xs">
-          <div className="flex flex-row items-center justify-end gap-2">
-            <span>{item.node.expectedDeliveryDate}</span>
-            <h4 className="text-tuscany"><RiTruckFill /></h4>
-          </div>
-          <div className="flex flex-row items-center justify-end gap-2">
-            <span>{item.node.orderDate}</span>
-            <h4 className="text-tuscany"><RiMoneyPoundBoxFill /></h4>
-          </div>
-          {item.node.orderId ?
-            <div className="flex flex-row items-center justify-end gap-2">
-              <span className="">{item.node.orderId}</span>
-              <h4 className="text-tuscany"><BiHash /></h4>
-            </div>
-            : null}
-        </div>
       </div>
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-8">
 
-        <h5 className="text-navy"><BsInfoCircleFill /></h5>
+        <h5 className="flex flex-row items-center gap-4 text-navy"><BsInfoCircleFill className="mt-1" /> <span className="opacity-10">Details</span></h5>
         {item.node.standard ? <p className="italic opacity-30 text-2xs">Standard order for this supplier</p> :
           <div
             className="text-2xs content"
@@ -44,7 +44,7 @@ export default function OrderCard({ item }) {
         }
         {item.node.notes ?
           <>
-            <h5 className="pt-4 border-t text-butterscotch-400 border-grey-200"><BsExclamationTriangleFill /></h5>
+            <h5 className="flex flex-row items-center gap-4 pt-8 border-t border-gray-100 text-butterscotch"><BsExclamationTriangleFill className="mt-1" /> <span className="opacity-30">Notes</span></h5>
             <div
               className="text-2xs content"
               dangerouslySetInnerHTML={{ __html: item.node.notes }} /></>
