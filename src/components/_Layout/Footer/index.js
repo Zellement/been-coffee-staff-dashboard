ca63ab8f28afd64eab6dccc891462128fec6ac80
Dan Farrow <dan@zellement.com>
2023-08-01 23:15:03 +0100

    add target

--- src/components/_Layout/Footer/index.js
+++ src/components/_Layout/Footer/index.js
@@ -14,7 +14,7 @@ export default function Footer() {
   return (
     isAuthenticated && (
       <footer
-        className={`fixed bottom-0 left-0 w-full bg-butterscotch border-t border-opacity-25 transition-all duration-300 border-navy-100 ${quickLinksActive
+        className={`fixed bottom-0 left-0 w-full bg-butterscotch border-t border-opacity-25 transition-all duration-300 border-navy-100 z-30 ${quickLinksActive
             ? `translate-y-0 lg:translate-y-full`
             : `translate-y-full lg:translate-y-0`
           }`}
